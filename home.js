df0301f85cb4737ebb07068e49829735c290dda9
viveroa2291 <viveroa2291@uwec.edu>
2022-06-03 17:30:12 -0500

    Update
    Made some minor changes in how the framework of the website is.

--- home.js
+++ home.js
@@ -3,8 +3,13 @@ const nextButton = document.getElementById('next-button'); // The next button
 const headerDiv = document.getElementById('header');
 const descriptionDiv = document.getElementById('descriptionBody');
 
-const headerTitles = ["Home", "About", "Contact", "Story", "Projects"];
-const headerDescritpions = ["This is the home page", "This is the about page", "This is the contact page", "This is the story page", "This is the projects page."];
+const headerTitles = ["Welcome", "About", "Contact", "Story", "Travel", "Projects"];
+const headerDescritpions = ["Welcome to my website. <br> This is the <a class='home-links' href='index.html'>Home</a> page where there is a guide on the left and a timeline on the right. <br> The Guide provides an overview about the website, whereas the Timeline displays my achievements throughout my life.",
+"In the <a class='home-links' href='about.html'>About</a> page, it dives into further in my life on a deeper level where you can read about me. <br> It can be referenced as a biography.", 
+"This is the contact page", 
+"This is the story page", 
+"This is the travel page", 
+"This is the projects page."];
 const linkTitles = ["home.html", "about.html", "contact.html", "story.html", "projects.html"];
 let s = 0;
 
